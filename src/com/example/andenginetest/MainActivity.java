9610eff692990c068356c62d1a2840a968904658
Nam gunbaek <rjsqor@gmail.com>
2014-05-27 21:05:46 +0900

    NewJoyStic

--- src/com/example/andenginetest/MainActivity.java
+++ src/com/example/andenginetest/MainActivity.java
@@ -19,37 +19,26 @@ public class MainActivity extends SimpleBaseGameActivity
  // 카메라 필드를 생성합니다.
  // 가로와 세로 2개의 카메라 변수를 선언했습니다.
  private Camera camera;
- private static final int CAMERA_WIDTH = 720;
- private static final int CAMERA_HEIGHT = 480;
+ private static int CAMERA_WIDTH = 720;
+ private static int CAMERA_HEIGHT = 480;
  
  private BitmapTextureAtlas BtaJoysticBg;
  private BitmapTextureAtlas BtaJoysticPoint;
- 
  private BitmapTextureAtlas mBitmapTextureAtlas;
- private BitmapTextureAtlas BtaBtnMoveR;
- private BitmapTextureAtlas BtaBtnMoveL;
- private BitmapTextureAtlas BtaBtnMoveU;
- private BitmapTextureAtlas BtaBtnMoveD;
  
  private TiledTextureRegion TrJoysticBg;
  private TiledTextureRegion TrJoysticPoint;
  private TiledTextureRegion mPlayerTextureRegion;
- private TiledTextureRegion TrBtnMoveR;
- private TiledTextureRegion TrBtnMoveL;
- private TiledTextureRegion TrBtnMoveU;
- private TiledTextureRegion TrBtnMoveD;
- 
- private boolean isMoveR = false;
- private boolean isMoveL = false;
- private boolean isMoveU = false;
- private boolean isMoveD = false;
+
  private boolean isClick = false;
- 
- private Sprite btnMoveR, btnMoveL, btnMoveU, btnMoveD;
  
  @Override
  public EngineOptions onCreateEngineOptions()
  {
+	 DisplayMetrics displayMetrics = new DisplayMetrics();
+	 getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+	 CAMERA_WIDTH = displayMetrics.widthPixels;
+	 CAMERA_HEIGHT = displayMetrics.heightPixels;
   camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
   EngineOptions engineOptions = new EngineOptions(true,
     ScreenOrientation.LANDSCAPE_FIXED, new FillResolutionPolicy(), camera);
@@ -66,31 +55,15 @@ public class MainActivity extends SimpleBaseGameActivity
 			 BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset
 			 (this.mBitmapTextureAtlas, this, "player.png", 0, 0, 3, 1);
      this.mBitmapTextureAtlas.load();
-     
-     //버튼 BTA 생성
-     BtaBtnMoveD =  new BitmapTextureAtlas(getTextureManager(), 500, 250, TextureOptions.BILINEAR);
-     BtaBtnMoveR =  new BitmapTextureAtlas(getTextureManager(), 500, 250, TextureOptions.BILINEAR);
-     BtaBtnMoveL =  new BitmapTextureAtlas(getTextureManager(), 500, 250, TextureOptions.BILINEAR);
-     BtaBtnMoveU =  new BitmapTextureAtlas(getTextureManager(), 500, 250, TextureOptions.BILINEAR);
-     
-     //버튼 TR 생성
-     TrBtnMoveD = new BitmapTextureAtlasTextureRegionFactory()
-     .createTiledFromAsset(BtaBtnMoveD, this, "btnDown.png", 0, 0, 2, 1);
-     TrBtnMoveR = new BitmapTextureAtlasTextureRegionFactory()
-     .createTiledFromAsset(BtaBtnMoveR, this, "btnRight.png", 0,0, 2, 1);
-     TrBtnMoveL = new BitmapTextureAtlasTextureRegionFactory().createTiledFromAsset(BtaBtnMoveL, this, "btnLeft.png", 0,0, 2, 1);
-     TrBtnMoveU = new BitmapTextureAtlasTextureRegionFactory().createTiledFromAsset(BtaBtnMoveU, this, "btnUp.png", 0,0, 2, 1);
-     
-     BtaBtnMoveD.load();
-     BtaBtnMoveL.load();
-     BtaBtnMoveR.load();
-     BtaBtnMoveU.load();
-     
+        
      //Joystic 생성
      BtaJoysticBg = new BitmapTextureAtlas(getTextureManager(), 500, 500, TextureOptions.BILINEAR);
      BtaJoysticPoint = new BitmapTextureAtlas(getTextureManager(), 50, 50, TextureOptions.BILINEAR);
      TrJoysticBg = new BitmapTextureAtlasTextureRegionFactory().createTiledFromAsset(BtaJoysticBg, this, "joysticBg.png", 0, 0, 1,1);
      TrJoysticPoint = new BitmapTextureAtlasTextureRegionFactory().createTiledFromAsset(BtaJoysticPoint, this, "joysticPoint.png", 0, 0, 1,1);
+     
+     BtaJoysticBg.load();
+     BtaJoysticPoint.load();
      
  }
  
@@ -112,160 +85,73 @@ public class MainActivity extends SimpleBaseGameActivity
   player.setScaleCenterY(this.mPlayerTextureRegion.getHeight());
   player.setScale(2);
   player.animate(new long[]{200, 200, 200}, 0, 2, true);
-  
   scene.attachChild(player);
 
-
-  //btn setting
-  Sprite SpriteBtnMoveU = new Sprite(100, CAMERA_HEIGHT - 220, 80, 80, TrBtnMoveU, vertexBufferObjectManager){
-	  @Override
-	  public boolean onAreaTouched(final TouchEvent TouchE, final float TouchX, final float TouchY) {
-		 if(TouchE.getAction() == TouchEvent.ACTION_DOWN){
-			 isClick = true;
-			 isMoveU = true;
-			 Log.e("btnClick", "U_DOWN");
-		 }else if(TouchE.getAction() == TouchEvent.ACTION_UP){
-			 isClick = false;
-			 isMoveU = false;;
-			 Log.e("btnClick", "U_UP");
-		 }
-		  return false;
-	  }  
-	  @Override
-	  protected void onManagedUpdate(float pSecondsElapsed) {
-		  int minY = 0;
-		  int maxY = (int)getEngine().getCamera().getHeight();
-		  
-		  if(isClick && isMoveU && player.getY()-player.getHeight() >= minY){
-			  Log.e("onManagedUpdate", "U");
-			  player.setPosition(player.getX(), player.getY()-10);
-		  }
-		  super.onManagedUpdate(pSecondsElapsed);
-	  };
-  };
-  Sprite SpriteBtnMoveD = new Sprite(100, CAMERA_HEIGHT - 100, 80, 80, TrBtnMoveD, vertexBufferObjectManager){
-	  @Override
-	  public boolean onAreaTouched(final TouchEvent TouchE, final float TouchX, final float TouchY) {
-		 if(TouchE.getAction() == TouchEvent.ACTION_DOWN){
-			 isClick = true;
-			 isMoveD = true;
-			 Log.e("btnClick", "D_DOWN");
-		 }else if(TouchE.getAction() == TouchEvent.ACTION_UP){
-			 isClick = false;
-			 isMoveD = false;;
-			 Log.e("btnClick", "D_UP");
-		 }
-		  return false;
-	  }  
-	  @Override
-	  protected void onManagedUpdate(float pSecondsElapsed) {
-		  int minY = 0;
-		  int maxY = (int)getEngine().getCamera().getHeight();
-		 
-		  if(isClick && isMoveD && player.getY()+player.getHeight() <= maxY){
-			  Log.e("onManagedUpdate", "D");
-			  player.setPosition(player.getX(), player.getY()+10);
-		  }
-		  super.onManagedUpdate(pSecondsElapsed);
-	  };
-  };
-  Sprite SpriteBtnMoveL = new Sprite(20, CAMERA_HEIGHT - 160, 80, 80, TrBtnMoveL, vertexBufferObjectManager){
-	  @Override
-	  public boolean onAreaTouched(final TouchEvent TouchE, final float TouchX, final float TouchY) {
-		 if(TouchE.getAction() == TouchEvent.ACTION_DOWN){
-			 isClick = true;
-			 isMoveL = true;
-			 Log.e("btnClick", "L_DOWN");
-		 }else if(TouchE.getAction() == TouchEvent.ACTION_UP){
-			 isClick = false;
-			 isMoveL = false;
-			 Log.e("btnClick", "L_UP");
-		 }
-		  return false;
-	  }  
-	  @Override
-	  protected void onManagedUpdate(float pSecondsElapsed) {
-		  int minX = 0;
-		  int maxX = (int)getEngine().getCamera().getWidth();
-		  
-		  if(isClick && isMoveL && player.getX()-player.getWidth() >= minX){
-			  Log.e("onManagedUpdate", "L");
-			  player.setPosition(player.getX()-10, player.getY());
-		  }
-		  
-		  super.onManagedUpdate(pSecondsElapsed);
-	  };
-  };
-  Sprite SpriteBtnMoveR = new Sprite(180, CAMERA_HEIGHT - 160, 80, 80, TrBtnMoveR, vertexBufferObjectManager){
-	  @Override
-	  public boolean onAreaTouched(final TouchEvent TouchE, final float TouchX, final float TouchY) {
-		 if(TouchE.getAction() == TouchEvent.ACTION_DOWN){
-			 isClick = true;
-			 isMoveR = true;
-			 Log.e("btnClick", "R_DOWN");
-		 }else if(TouchE.getAction() == TouchEvent.ACTION_UP){
-			 isClick = false;
-			 isMoveR = false;;
-			 Log.e("btnClick", "R_UP");
-		 }
-		  return false;
-	  }  
-	  @Override
-	  protected void onManagedUpdate(float pSecondsElapsed) {
-		  int minX = 0;
-		  int maxX = (int)getEngine().getCamera().getWidth() - (int)player.getWidth();
-		  
-		  if(isClick && isMoveR && player.getX()+player.getWidth() <= maxX){
-			  Log.e("onManagedUpdate", "R");
-			  player.setPosition(player.getX()+10, player.getY());
-		  }
-		  super.onManagedUpdate(pSecondsElapsed);
-	  };
-  };
-
-  final float JoysticBgWidth = 200;
-  final float JoysticBgHeight = 200;
+  final float JoysticBgWidth = 400;
+  final float JoysticBgHeight = 400;
   final float JoysticBgMerginX = 100;
   final float JoysticBgMerginY = 100;
   final float JoysticBgLeft = CAMERA_WIDTH - JoysticBgWidth - JoysticBgMerginX;
   final float JoysticBgRight = CAMERA_WIDTH - JoysticBgMerginX; 
   final float JoysticBgTop = CAMERA_HEIGHT - JoysticBgMerginY - JoysticBgHeight;
-  final float JoysticBgButtom = CAMERA_HEIGHT - JoysticBgMerginY ;
-  final Sprite SpriteJoysticPoint = new Sprite(0,0,25,25,TrJoysticPoint, vertexBufferObjectManager);
-  
+  final float JoysticBgBottum = CAMERA_HEIGHT - JoysticBgMerginY ;
+  final Sprite SpriteJoysticPoint = new Sprite(0,0,50,50,TrJoysticPoint, vertexBufferObjectManager);
+
   Sprite SpriteJoysticBg = new Sprite(JoysticBgLeft, JoysticBgTop, JoysticBgWidth, JoysticBgHeight, TrJoysticBg, vertexBufferObjectManager){
+	  float MoveX = 0;
+	  float MoveY = 0;
+	  int minX = 0;
+	  int maxX = (int)getEngine().getCamera().getWidth() - (int)player.getWidth();
+	  int minY = 0;
+	  int maxY = (int)getEngine().getCamera().getHeight();
+	  
 	  @Override
 	  public boolean onAreaTouched(final TouchEvent TouchE, final float TouchX, final float TouchY) {
-		  if(TouchX >= JoysticBgLeft && TouchX <= JoysticBgRight 
-				  && TouchY >= JoysticBgTop && TouchY <= JoysticBgButtom){
-			  SpriteJoysticPoint.setPosition(TouchY - SpriteJoysticPoint.getWidth()/2, TouchY - SpriteJoysticPoint.getHeight()/2);
-			  SpriteJoysticPoint.setVisible(true);
-			  
-		  }
+		 	 Log.i("SpriteJoysticBg","Touch X:"+TouchX+" Y:"+TouchY);
+
+			  MoveX = (TouchX - (JoysticBgWidth/2))/10;
+			  MoveY = (TouchY - (JoysticBgHeight/2))/10;
+		
+		 	 SpriteJoysticPoint.setPosition(JoysticBgLeft + TouchX - SpriteJoysticPoint.getWidth()/2,JoysticBgTop + TouchY - SpriteJoysticPoint.getHeight()/2);
+			 SpriteJoysticPoint.setVisible(true);
+			 isClick = true;
+		 	 if(TouchE.getAction() == TouchEvent.ACTION_UP){
+				  SpriteJoysticPoint.setVisible(false);
+				  isClick = false;
+			 }
 		  return false;
 	  }  
 	  @Override
 	  protected void onManagedUpdate(float pSecondsElapsed) {
-
+		  if(isClick){
+			  if(player.getX() + MoveX < minX){
+				  player.setPosition(minX, player.getY());
+			  }else if(player.getX() + MoveX > maxX){
+				  player.setPosition(maxX, player.getY());
+			  }else if(player.getX() >= minX && player.getX() <= maxX){
+				  player.setPosition(player.getX() + MoveX, player.getY());
+			  }
+			  if(player.getY() + MoveY < minY){
+				  player.setPosition(player.getX(), minY);
+			  }else if(player.getY() + MoveY > maxY){
+				  player.setPosition(player.getX(), maxY);
+			  }else if(player.getY() >= minY && player.getY() <= maxY){
+				  player.setPosition(player.getX(), player.getY()+MoveY);
+			  }
+		  }
 		  super.onManagedUpdate(pSecondsElapsed);
 	  };
   };
  
-  
-  scene.attachChild(SpriteBtnMoveR);
-  scene.attachChild(SpriteBtnMoveL);
-  scene.attachChild(SpriteBtnMoveU);
-  scene.attachChild(SpriteBtnMoveD);
-  
-  scene.registerTouchArea(SpriteBtnMoveR);
-  scene.registerTouchArea(SpriteBtnMoveL);
-  scene.registerTouchArea(SpriteBtnMoveU);
-  scene.registerTouchArea(SpriteBtnMoveD);
 
-  SpriteJoysticPoint.setVisible(false);
-  scene.attachChild(SpriteJoysticPoint);
   scene.attachChild(SpriteJoysticBg);
+  scene.attachChild(SpriteJoysticPoint);
+
+  SpriteJoysticPoint.setZIndex(5);
+  SpriteJoysticBg.setZIndex(0);
+  
   scene.registerTouchArea(SpriteJoysticBg);
+  SpriteJoysticPoint.setVisible(false);
   scene.setTouchAreaBindingOnActionDownEnabled(true);
   
   
